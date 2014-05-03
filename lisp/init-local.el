c08776111d45a850d9e5521785ef95cb76c0e67e
George <gaoshiming@live.com>
2014-05-04 01:06:25 +0800

    add indent level

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,7 +5,20 @@
 
 (setq js2-basic-offset preferred-indent-level
       js-indent-level preferred-indent-level
-      coffee-tab-width preferred-indent-level)
+      coffee-tab-width preferred-indent-level
+      sgml-basic-offset preferred-indent-level
+      sh-basic-offset preferred-indent-level
+      c-basic-offset preferred-indent-level
+      ruby-indent-level preferred-indent-level
+      css-indent-offset preferred-indent-level
+      )
 
+;; add w3m support
+(require-package 'w3m)
+
+;; set evernote-mode not work now
+;;TODO make it work
+;;(require-package 'evernote-mode)
+;; (setq evernote-developer-token "")
 
 (provide 'init-local)
