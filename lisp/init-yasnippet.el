2b9a79b96be1fa18ba5f5809b69184703147b855
gsmlg <gsmlg.com@gmail.com>
2014-06-08 19:28:36 +0800

    fix bug of list in yasnippet

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,6 +1,8 @@
 ;; yasnippet install and settings
 (require-package 'yasnippet)
 
+(defvar yas-snippet-dirs (list (expand-file-name "snippets" user-emacs-directory)))
+
 (add-to-list 'yas-snippet-dirs (expand-file-name "yasnippet-snippets" user-emacs-directory))
 (add-to-list 'yas-snippet-dirs (expand-file-name "yasmate/snippets" user-emacs-directory))
 
