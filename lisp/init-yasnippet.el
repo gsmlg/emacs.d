3d081acd4244d2612d7f7f671c006b355f7a344d
gsmlg <gsmlg.com@gmail.com>
2014-06-08 18:22:51 +0800

    add yasnippet's snippets repo from github

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,7 +1,9 @@
 ;; yasnippet install and settings
 (require-package 'yasnippet)
+
+(add-to-list 'yas-snippet-dirs (expand-file-name "yasnippet-snippets" user-emacs-directory))
+(add-to-list 'yas-snippet-dirs (expand-file-name "yasmate/snippets" user-emacs-directory))
+
 (yas-global-mode 1)
-
-
 
 (provide 'init-yasnippet)
