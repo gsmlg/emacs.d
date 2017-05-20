37953929cc1459b685cf7b8174e5aea8e668dd71
gsmlg <gsmlg@gsmlg.com>
2017-05-20 13:56:07 +0800

    restructure my emacs config files

---
+++ init.el
@@ -0,0 +1,54 @@
+
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+
+(when (version<= emacs-version "24.4")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; Calls (package-initialize)
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+
+;; Extra packages which don't require any configuration
+
+;;----------------------------------------------------------------------------
+;; Allow access from emacsclient
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(require 'init-local nil t)
+
+
+(provide 'init)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
---
+++ lisp/init-local.el
@@ -0,0 +1,52 @@
+;; init-local local settings
+
+;; key bindings
+;; remap help , help is at <F1>
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; add editorconfig plugin for projects
+(require-package 'editorconfig)
+
+;; set global-mode-string
+;(add-to-list 'global-mode-string  current-time-string)
+
+;; set gnus news group
+;(setq gnus-select-method '(nntp "news.supernews.com"))
+
+
+
+;; set indent level for all mode
+(setq preferred-indent-level 4)
+
+(defun gsmlg/set-indent (&optional width)
+  "set the indent of each language mode
+now in js js2 coffeescript sgml(html,xml) sh(shell) c ruby css
+should be set as samewidth"
+  (interactive)
+  (let ((indent-width (or width preferred-indent-level)))
+  (setq js2-basic-offset indent-width
+        js2-indent-switch-body indent-width
+        js-indent-level indent-width
+        jsx-indent-level indent-width
+        coffee-tab-width indent-width
+        sgml-basic-offset indent-width
+        sh-basic-offset indent-width
+        c-basic-offset indent-width
+        ruby-indent-level indent-width
+        css-indent-offset indent-width
+        yaml-indent-offset indent-width
+        )))
+
+;; run indent settings first time
+(gsmlg/set-indent preferred-indent-level)
+
+;; add w3m support
+(require-package 'w3m)
+
+;; set evernote-mode not work now
+;;TODO make it work
+;;(require-package 'evernote-mode)
+;; (setq evernote-developer-token "")
+
+
+(provide 'init-local)
