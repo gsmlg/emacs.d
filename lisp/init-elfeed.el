3258b8976208922492ce11f348eae5781691adcd
gsmlg <gsmlg@gsmlg.com>
2017-11-26 14:26:40 +0800

    fix elfeed and helm-projectile

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -3,6 +3,7 @@
 
 (elfeed-goodies/setup)
 
+(setq elfeed-db-directory "~/.emacs.d/elfeed")
 (setq elfeed-feeds
       '(("http://blog.atom.io/feed.xml" blog editor)
         ("http://www.ruanyifeng.com/blog/atom.xml" blog technology)))
