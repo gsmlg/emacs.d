faaf1a2ed13eb117e378bb89e5c6ac1ab857d659
gsmlg <gsmlg@gsmlg.com>
2017-12-01 01:14:34 +0800

    add neotree and use doom-one-dark-theme

---
+++ lisp/init-neotree.el
@@ -0,0 +1,28 @@
+;; neotree config
+;; A Emacs tree plugin like NerdTree for Vim.
+
+(require-package 'neotree)
+
+;;(global-set-key [f8] 'neotree-toggle)
+
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;; Every time when the neotree window is opened, let it find current file and jump to node.
+(setq neo-smart-open t)
+;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically.
+(setq projectile-switch-project-action 'neotree-projectile-action)
+;; use with projectile
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
+(global-set-key [f8] 'neotree-project-dir)
+
+(provide 'init-neotree)
