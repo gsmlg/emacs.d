ac2bb22b2e3f111d24112bca155e3a660946a333
gsmlg <gsmlg@gsmlg.com>
2017-12-22 22:32:25 +0800

    init spacemacs

---
+++ init.el
@@ -0,0 +1,21 @@
+;; -*- lexical-binding: t -*-
+
+;;; this config files import spacemacs
+(setenv "SPACEMACSDIR" "~/.emacs.d/lisp/")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
+(load-file (concat spacemacs-start-directory "init.el"))
+
+
+(provide 'init)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
