07466cbdba763cad4b6588978793aace2a4a2851
gsmlg <me@gsmlg.org>
2020-08-31 17:41:16 +0800

    update mu4e

--- site-lisp/mu4e/mu4e-meta.el
+++ site-lisp/mu4e/mu4e-meta.el
@@ -1,11 +1,11 @@
 ;; auto-generated
-(defconst mu4e-mu-version "0.9.18"
+(defconst mu4e-mu-version "1.4.13"
   "Required mu binary version; mu4e's version must agree with this.")
 
-(defconst mu4e-builddir "/private/tmp/mu-20171223-85354-17ixc8j/mu-0.9.18"
+(defconst mu4e-builddir "/private/tmp/mu-20200812-31877-uov0xm/mu-1.4.13"
   "Top-level build directory.")
 
-(defconst mu4e-doc-dir "/usr/local/Cellar/mu/0.9.18_1/share/doc/mu"
+(defconst mu4e-doc-dir "/usr/local/Cellar/mu/1.4.13/share/doc/mu"
   "Mu4e's data-dir.")
 
 (provide 'mu4e-meta)
